6517fcced6a7f3491f59e362b2eb64305629af1a
Salvador Guzman <guzmansalv@gmail.com>
2018-10-01 05:24:34 -0700

    working

---
+++ src/alabama-email.py
@@ -0,0 +1,31 @@
+import requests
+import bs4
+
+
+url = 'http://emason.alafreemasonry.org/?empf=Search'
+
+for i in range(65, 91):
+    char = chr(i)
+
+    params = {
+        'first': char,
+        'search': '1'
+    }
+
+    req = requests.post(url, data=params)
+    soup = bs4.BeautifulSoup(req.text, 'html.parser')
+
+    cells = soup.findAll('td')
+    for td in cells:
+        a = td.contents[0]
+        href = a['href']
+
+        person_url = f'http://emason.alafreemasonry.org/{href}'
+        person_req = requests.get(person_url)
+        person_soup = bs4.BeautifulSoup(person_req.text, 'html.parser')
+
+        para = person_soup.select_one('p > a > span')
+        name = para.text
+        print(name)
+
+
